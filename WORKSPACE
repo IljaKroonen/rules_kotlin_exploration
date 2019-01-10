ea7379b8e21252d012c50c35ce4a7eebde3ddb94
Ilja Kroonen <IljaKroonen@users.noreply.github.com>
2019-01-10 22:20:27 +0100

    Normalized jars (#1)
    Normalize jars using JarCreator

--- WORKSPACE
+++ WORKSPACE
@@ -1,1 +1,15 @@
 workspace(name = "com_github_iljakroonen_rules_kotlin")
+
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive", "http_file")
+
+http_file(
+    name = "io_bazel_jarhelper_java",
+    urls = ["https://raw.githubusercontent.com/bazelbuild/bazel/dc8fea0d7fce9f6aaa7b971d1bf2d81895c77fac/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/jarhelper/JarHelper.java"],
+    sha256 = "3b8f0e350ac185fe5783d768845eb458e5c6365bf4c57bbcd01850e3262136cd",
+)
+
+http_file(
+    name = "io_bazel_jarcreator_java",
+    urls = ["https://raw.githubusercontent.com/bazelbuild/bazel/dc8fea0d7fce9f6aaa7b971d1bf2d81895c77fac/src/java_tools/buildjar/java/com/google/devtools/build/buildjar/jarhelper/JarCreator.java"],
+    sha256 = "eacb422a5aa1e256fd263354d0dedcff6d95c378e5534d4778d0390fd41f6978",
+)
---
+++ src/main/java/com/github/iljakroonen/rules/kotlin/CompileKotlin.java
@@ -0,0 +1,59 @@
+package com.github.iljakroonen.rules.kotlin;
+
+import com.google.devtools.build.buildjar.jarhelper.JarCreator;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CompileKotlin {
+    public static void main(String[] args) {
+        try {
+            String kotlincExecutablePath = args[0];
+            String jdkHome = args[1];
+            String[] kotlinSources = args[2].split(System.getProperty("path.separator"));
+            String compileClasspath = args[3];
+            Path outputJar = Paths.get(args[4]);
+            String moduleName = args[5];
+
+            Path tempCompilationDirectory = Files.createTempDirectory("kotlinc_outputs");
+
+            String[] kotlincFlags = new String[]{
+                    "-d",
+                    tempCompilationDirectory.toString(),
+                    "-cp",
+                    compileClasspath,
+                    "-jdk-home",
+                    jdkHome,
+                    "-module-name",
+                    moduleName
+            };
+
+            ArrayList<String> kotlincArgs = new ArrayList<>();
+            kotlincArgs.add(kotlincExecutablePath);
+            kotlincArgs.addAll(Arrays.asList(kotlinSources));
+            kotlincArgs.addAll(Arrays.asList(kotlincFlags));
+
+            Process process = new ProcessBuilder()
+                    .command(kotlincArgs)
+                    .redirectError(ProcessBuilder.Redirect.INHERIT).redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+
+            int outCode = process.waitFor();
+
+            if (outCode != 0)
+                System.exit(outCode);
+
+            JarCreator jarCreator = new JarCreator(outputJar);
+            jarCreator.addDirectory(tempCompilationDirectory);
+            jarCreator.setCompression(true);
+            jarCreator.setNormalize(true);
+            jarCreator.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
